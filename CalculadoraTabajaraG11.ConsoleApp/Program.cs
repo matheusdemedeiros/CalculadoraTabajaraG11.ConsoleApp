a1eb9a1151f9e2a50fa80f1d75bb05d0e7d49a47
Matheus <Matheus@MATHEUS>
2022-06-15 14:36:16 -0300

    Operação de subtração implementada com sucesso.

--- CalculadoraTabajaraG11.ConsoleApp/Program.cs
+++ CalculadoraTabajaraG11.ConsoleApp/Program.cs
@@ -24,13 +24,68 @@ namespace CalculadoraTabajaraG11.ConsoleApp
                 Console.WriteLine("Digite S para sair\n");
 
                 Console.Write("Opção: ");
+
                 opcao = Console.ReadLine();
 
                 if (opcao.Equals("s", StringComparison.OrdinalIgnoreCase))
                     break;
+
+                #endregion
+
+                #region Realizar Cálculo
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                string operacao = "";
+
+                switch (opcao)
+                {
+                    case "2": operacao = "Subtração"; break;
+
+                    default: break;
                 }
+
+                string subtitulo = $"Novo calculo de {operacao}";
+
+                Console.WriteLine(subtitulo + "\n");
+
+                Console.Write($"Digite o primeiro número: ");
+                double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write($"Digite o segundo número: ");
+                double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+                double resultado = 0;
+
+                switch (opcao)
+                {
+                    case "2": resultado = primeiroNumero - segundoNumero; break;
+
+                    default: break;
+                }
+
+                #endregion
+
+                #region Mostrar Resultado
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                Console.WriteLine("Tela de Resultados\n");
+
+                Console.Write($"O resultado da operação da {operacao} é: {resultado}");
+
+                Console.WriteLine();
+
+                Console.ReadLine();
+
+                Console.Clear();
 
                 #endregion
             }
         }
+    }
 }
