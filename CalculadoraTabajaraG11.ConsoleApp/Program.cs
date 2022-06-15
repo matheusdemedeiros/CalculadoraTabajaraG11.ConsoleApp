b3f598b3a94874933cc31f1aa839ec6a267646a1
Matheus <Matheus@MATHEUS>
2022-06-15 13:56:24 -0300

    Add project files.

---
+++ CalculadoraTabajaraG11.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculadoraTabajaraG11.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
