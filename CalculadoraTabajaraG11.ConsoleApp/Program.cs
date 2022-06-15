9ae381c25abfa549a6bdd55f8cf52c71f8a3a6e5
Matheus <Matheus@MATHEUS>
2022-06-15 14:18:30 -0300

    Adicionando a opção de subtração no menu principal

--- CalculadoraTabajaraG11.ConsoleApp/Program.cs
+++ CalculadoraTabajaraG11.ConsoleApp/Program.cs
@@ -4,9 +4,33 @@ namespace CalculadoraTabajaraG11.ConsoleApp
 {
     public class Program
     {
+        //2º Deve realizar operações de subtração
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcao;
+
+            while (true)
+            {
+                #region Menu Principal
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                Console.WriteLine("Tela Principal\n");
+
+                Console.WriteLine("Digite 2 para realizar operações de Subtração\n");
+
+                Console.WriteLine("Digite S para sair\n");
+
+                Console.Write("Opção: ");
+                opcao = Console.ReadLine();
+
+                if (opcao.Equals("s", StringComparison.OrdinalIgnoreCase))
+                    break;
+                }
+
+                #endregion
+            }
         }
-    }
 }
