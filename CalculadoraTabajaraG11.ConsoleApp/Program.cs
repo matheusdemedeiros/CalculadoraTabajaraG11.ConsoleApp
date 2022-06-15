19ea88f4ccd1ccde1d7cbe0708169ad950ba97c5
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2022-06-15 15:15:20 -0300

    Implementção da adição

--- CalculadoraTabajaraG11.ConsoleApp/Program.cs
+++ CalculadoraTabajaraG11.ConsoleApp/Program.cs
@@ -1,12 +1,100 @@
-﻿using System;
+﻿
+using System;
 
 namespace CalculadoraTabajaraG11.ConsoleApp
 {
+
     public class Program
     {
+        //2º Deve realizar operações de subtração
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcao;
+
+            while (true)
+            {
+                #region Menu Principal
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                Console.WriteLine("Tela Principal\n");
+
+                Console.WriteLine("Digite 1 para realizar operações de Adição\n");
+
+                Console.WriteLine("Digite 2 para realizar operações de Subtração\n");
+
+                Console.WriteLine("Digite S para sair\n");
+
+                Console.Write("Opção: ");
+
+                opcao = Console.ReadLine();
+
+                if (opcao.Equals("s", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                #endregion
+
+                #region Realizar Cálculo
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                string operacao = "";
+
+                switch (opcao)
+                {
+
+                    case "1": operacao = "Adição"; break;
+
+                    case "2": operacao = "Subtração"; break;
+
+                    default: break;
+                }
+
+                string subtitulo = $"Novo calculo de {operacao}";
+
+                Console.WriteLine(subtitulo + "\n");
+
+                Console.Write($"Digite o primeiro número: ");
+                double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write($"Digite o segundo número: ");
+                double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+                double resultado = 0;
+
+                switch (opcao)
+                {
+                    case "1": resultado = primeiroNumero + segundoNumero; break;
+
+                    case "2": resultado = primeiroNumero - segundoNumero; break;
+
+                    default: break;
+                }
+
+                #endregion
+
+                #region Mostrar Resultado
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                Console.WriteLine("Tela de Resultados\n");
+
+                Console.Write($"O resultado da operação da {operacao} é: {resultado}");
+
+                Console.WriteLine();
+
+                Console.ReadLine();
+
+                Console.Clear();
+
+                #endregion
+            }
         }
     }
 }
